b75d42e73139f200f57c71de932767e24d42cc88
Hotcers <venezuelavvv01@protonmail.com>
2022-09-26 17:35:58 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,41 @@
+public class App {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setEdad(36);
+        persona.setNombre("David");
+        persona.setTelefono(3186796130l);
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getTelefono());
+    }
+}
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private long telefono;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+}
